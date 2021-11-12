f874a1fc3254087afac606a6fcb4ed3cb35ccd14
pablo-mz <pablo.mendez.nqn@gmail.com>
2021-11-12 14:04:30 +0100

    Add project files.

---
+++ Common.Lib/Authentication/User.cs
@@ -0,0 +1,11 @@
+﻿using Common.Lib.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Lib.Authentication
+{
+    public class User : Entity
+    {
+    }
+}
---
+++ MyLibrary.Lib/Models/Lend.cs
@@ -0,0 +1,11 @@
+﻿using Common.Lib.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary.Lib.Models
+{
+    public class Lend : Entity
+    {
+    }
+}
